88551b5a2cab946470f81ec7bea9edffa95a1cbb
TK <leandrotk100@gmail.com>
2019-01-07 23:07:28 -0200

    Configure project dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+
+                 ; Web
+                 [prismatic/schema "1.1.9"]
+                 [metosin/compojure-api "2.0.0-alpha26"]
+                 [ring/ring-jetty-adapter "1.6.3"]
+
+                 ; Database
+                 [toucan "1.1.9"]
+                 [org.postgresql/postgresql "42.2.4"]
+
+                 ; Password Hashing
+                 [buddy/buddy-hashers "1.3.0"]]
+
   :main ^:skip-aot clojure-api-labs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
